f012dff0cb763fc68d4ae1c41097b8977a88caba
SuperNutboy <natthapong_ta66@live.rmutl.ac.th>
2023-08-16 19:17:28 +0700

    UPfawfsefafefa

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -21,3 +21,37 @@
     Output:
         112 91 65
 */
+#include <stdio.h>
+
+int main (){
+
+    int fst , sec , thd ;
+
+    printf( "Input[1] :" ) ;
+    scanf( "%d",&fst );
+    printf( "Input[2] :" ) ;
+    scanf( "%d",&sec );
+    printf( "Input[3] :" ) ;
+    scanf( "%d",&thd );
+
+    if (fst >= sec && fst >= thd ) {
+        if (sec >= thd ) {
+            printf( "%d %d %d" ,fst ,sec ,thd );
+        } else {
+            printf( "%d %d %d" ,fst ,thd ,sec );
+        }
+    } else if (sec >= fst && sec >=thd) {
+        if (fst >= thd) {
+            printf( "%d %d %d" ,sec ,fst ,thd);
+        } else {
+            printf( "%d %d %d" ,sec ,thd ,fst);
+        }
+    } else {
+        if (fst >= sec) {
+            printf( "%d %d %d" ,thd ,fst ,sec);
+        } else {
+            printf( "%d %d %d" ,thd ,sec ,fst);
+        }
+    }
+    return 0;
+}
